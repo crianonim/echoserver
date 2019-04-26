d03fdd3aed3b0302fc6db587ad8859c570291aa6
Jan Skowronski <crianonim@gmail.com>
2019-04-26 07:07:02 +0100

    delay feature implemented

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-
+// if ?delay=secs is specified then delay processing
+router.use((req,res,next)=>{
+  if (req.query.delay){
+    setTimeout(next,req.query.delay*1000)
+  }
+  else {
+    next();
+  }
+} )
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });

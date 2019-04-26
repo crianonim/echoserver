05ac49294ae5aa69b25d0953307c68154d49265b
Jan Skowronski <crianonim@gmail.com>
2019-04-26 07:00:20 +0100

    /api route added

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+const apiRouter = require('./routes/api')
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use('/'+config.mountpath,mountedRouter);
 
 mountedRouter.use(express.static(path.join(__dirname, 'public')));
 mountedRouter.use('/', indexRouter);
+mountedRouter.use('/api',apiRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
